59015f754e3246a1695090ad471a809b400fa6d7
cmoad <cmoad@d1f71bfd-d213-0410-9341-b469b6926fd1>
2004-12-23 18:58:47 +0000

    Preliminary support for osx.  Setup rquires specifying OpenGL framework.
    git-svn-id: svn://svn.code.sf.net/p/glewpy/code/trunk@39 d1f71bfd-d213-0410-9341-b469b6926fd1

---
+++ glewpy/setup-osx.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Pyrex.Distutils import build_ext
+setup(
+  name = "glewpy",
+  version = '0.4',
+  description = 'Python Wrapping of GLEW',
+  author = 'Charles Moad',
+  author_email = 'cmoad@indiana.edu',
+  url = 'http://glewpy.sourceforge.net/',
+  ext_package = 'glew',
+  packages = ['glew', 'glew.gl', 'glew.glx', 'glew.wgl'],
+  package_dir = {'glew': 'src/'},
+  ext_modules=[
+    Extension('glew', ['src/glew.pyx'], libraries = ['GLEW'], extra_link_args = ['-framework', 'OpenGL']),
+    Extension('gl.arb', ['src/gl/arb.pyx'], libraries = ['GLEW'], extra_link_args = ['-framework', 'OpenGL']),
+    Extension('gl.gl', ['src/gl/gl.pyx'], libraries = ['GLEW'], extra_link_args = ['-framework', 'OpenGL']),
+    Extension('gl.ingr', ['src/gl/ingr.pyx'], libraries = ['GLEW'], extra_link_args = ['-framework', 'OpenGL']),
+    Extension('gl.intel', ['src/gl/intel.pyx'], libraries = ['GLEW'], extra_link_args = ['-framework', 'OpenGL']),
+    Extension('gl.ktx', ['src/gl/ktx.pyx'], libraries = ['GLEW'], extra_link_args = ['-framework', 'OpenGL'])
+    ],
+  cmdclass = {'build_ext': build_ext}
+)
